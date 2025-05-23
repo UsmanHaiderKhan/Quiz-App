f1772c18ea5d19a253ca5f7ecfcbc7a62415cc90
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2025-05-24 00:29:28 +0200

    ✨🎉 Add : New Component For Answers

---
+++ src/components/Answers.jsx
@@ -0,0 +1,27 @@
+import { useRef } from 'react';
+export default function Answers({ answers, selectedAnswer, onSelect, answerState }) {
+    const shuffledAnswers = useRef();
+    if (!shuffledAnswers.current) {
+        shuffledAnswers.current = [...answers].sort(() => Math.random() - 0.5);
+    }
+    return (
+        <ul id="answers">
+            {shuffledAnswers.current.map((answer) => {
+                const isSelected = selectedAnswer === answer;
+                let cssClass = '';
+                if (answerState === 'answered' && isSelected) {
+                    cssClass = 'selected';
+                }
+                if ((answerState === 'incorrect' && answerState === 'correct') && isSelected) {
+                    cssClass = answerState;
+                }
+                return (
+                    <li key={answer} className="answer">
+                        <button onClick={() => onSelect(answer)} className={cssClass}>{answer}</button>
+                    </li>
+                )
+            })}
+
+        </ul>
+    )
+} 
